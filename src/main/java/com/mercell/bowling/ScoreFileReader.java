6e790c211bd50c16736c181dc79f589d0621a6b4
Babak Mehrabi <s.babak.mehrabi@gmail.com>
2020-09-02 16:31:03 +0430

    Creating a class for reading a single-line comma separated file and return an ArrayList of scores
    updating the JMeter version in the pom.xml

---
+++ src/main/java/com/mercell/bowling/ScoreFileReader.java
@@ -0,0 +1,31 @@
+package com.mercell.bowling;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class ScoreFileReader {
+
+    private String filePath;
+
+    public ScoreFileReader(String filePath) {
+        this.filePath = filePath;
+    }
+
+    public List<Integer> readFileAndExtractTheScores() {
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(filePath));
+            String line = br.readLine();
+            List<String> scoresAsStringList = Arrays.asList(line.replaceAll("\\s+","").split(","));
+            return scoresAsStringList.stream().map(Integer::parseInt).collect(Collectors.toList());
+        } catch (IOException e) {
+            System.err.println("Error: " + e);
+            return null;
+        }
+    }
+
+}
---
+++ src/test/java/com/mercell/bowling/ScoreFileReaderTest.java
@@ -0,0 +1,35 @@
+package com.mercell.bowling;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+class ScoreFileReaderTest {
+
+    ScoreFileReader scoreFileReader;
+
+    @Test
+    public void givenANonExistingFile_whenReadFileAndExtractScores_thenReturnNull() {
+        scoreFileReader = new ScoreFileReader("not-existed-file.csv");
+
+        List<Integer> scores = scoreFileReader.readFileAndExtractTheScores();
+
+        assertNull(scores);
+    }
+
+    @Test
+    public void givenValidFile_whenReadFileAndExtractScores_thenReturnAValidArrayListContainingAllScores() throws IOException {
+        List<Integer> scores = Arrays.asList(1, 2, 3, 4, 5, 6);
+
+        scoreFileReader = new ScoreFileReader("src\\test\\java\\resources\\score-files-for-test.txt");
+
+        assertArrayEquals(scores.toArray(), scoreFileReader.readFileAndExtractTheScores().toArray());
+    }
+
+}

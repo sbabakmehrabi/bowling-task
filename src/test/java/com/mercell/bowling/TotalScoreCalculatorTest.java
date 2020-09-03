37b3a439a2ba21beac6b58d63fbeaa3c301c4f20
Babak Mehrabi <s.babak.mehrabi@gmail.com>
2020-09-03 19:27:08 +0430

    Adding two other tests that are provided by task description. One of this test, shows us a bug in the implementation. When we receive a strike shot, we calculate our score base on the two shots in the next turn. If the next shot is a strike as well, we must look at the firstScore of the next shot or the bonusPoint1 (if the next shot is the last shot)

--- src/test/java/com/mercell/bowling/TotalScoreCalculatorTest.java
+++ src/test/java/com/mercell/bowling/TotalScoreCalculatorTest.java
@@ -70,4 +70,61 @@ class TotalScoreCalculatorTest {
 
         assertEquals(2 + 4 + 21, totalScoreCalculator.calculate());
     }
+
+    @Test
+    public void givenASequenceWith10ShotsProvidedByTaskDescription_whenCalculate_thenReturnTheCorrectSum() {
+        List<FrameScore> frameScores = new ArrayList<FrameScore>() {{
+            add(new FrameScore(0, 3));
+            add(new FrameScore(5, 0));
+            add(new FrameScore(9, 1));
+            add(new FrameScore(2, 5));
+            add(new FrameScore(3, 2));
+            add(new FrameScore(4, 2));
+            add(new FrameScore(3, 3));
+            add(new FrameScore(4, 6));
+            add(new FrameScore(10));
+            add(new FrameScore(10, -1, 2, 5));
+        }};
+        TotalScoreCalculator totalScoreCalculator = new TotalScoreCalculator(frameScores);
+
+        assertEquals(103, totalScoreCalculator.calculate());
+    }
+
+    @Test
+    public void givenASequenceWith10ShotsProvidedBySecondExampleInTaskDescription_whenCalculate_thenReturnTheCorrectSum() {
+        List<FrameScore> frameScores = new ArrayList<FrameScore>() {{
+            add(new FrameScore(7,1));
+            add(new FrameScore(5, 5));
+            add(new FrameScore(2, 7));
+            add(new FrameScore(4, 6));
+            add(new FrameScore(0, 5));
+            add(new FrameScore(8, 2));
+            add(new FrameScore(8, 1));
+            add(new FrameScore(4, 3));
+            add(new FrameScore(2, 4));
+            add(new FrameScore(5, 2));
+        }};
+        TotalScoreCalculator totalScoreCalculator = new TotalScoreCalculator(frameScores);
+
+        assertEquals(91, totalScoreCalculator.calculate());
+    }
+
+    @Test
+    public void givenASequenceWith10ShotsWith3StrikesShotAtTheLast3Turns_whenCalculate_thenReturnTheCorrectSum() {
+        List<FrameScore> frameScores = new ArrayList<FrameScore>() {{
+            add(new FrameScore(5, 0));
+            add(new FrameScore(5, 0));
+            add(new FrameScore(5, 0));
+            add(new FrameScore(5, 0));
+            add(new FrameScore(4, 0));
+            add(new FrameScore(8, 2));
+            add(new FrameScore(0, 6));
+            add(new FrameScore(10));
+            add(new FrameScore(10));
+            add(new FrameScore(10, -1, 8, 1));
+        }};
+        TotalScoreCalculator totalScoreCalculator = new TotalScoreCalculator(frameScores);
+
+        assertEquals(117, totalScoreCalculator.calculate());
+    }
 }
